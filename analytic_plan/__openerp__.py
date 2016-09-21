cf8b462d0b5bd657f5eb01ca0bad003900c3c5ac
Sarai Osorio <sarai.osorio@jarsa.com.mx>
2016-09-21 13:42:23 -0500

    [add][analytic_paln]- add view for model of analytic plan

--- analytic_plan/__openerp__.py
+++ analytic_plan/__openerp__.py
@@ -1,19 +1,21 @@
 # -*- coding: utf-8 -*-
 # © <2016> <Jarsa Sistemas, S.A. de C.V.>
-# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+# License LGPL-3.0 or later (https://www.gnu.org/licenses/lgpl.html).
 
 {
-    "name": "Analytic Plan",
-    "version": "9.0.0.1.0",
-    "category": "Project",
-    "author": "Jarsa Sistemas",
-    "website": "https://www.jarsa.com.mx",
-    "depends": [],
-    "license": "AGPL-3",
-    "data": [
+    'name': 'Analytic Plan',
+    'summary': '',
+    'version': '9.0.1.0.0',
+    'author': 'Jarsa Sistemas, S.A de C.V.',
+    'website': 'https://www.jarsa.com.mx',
+    'category': 'Warehouse Management',
+    'license': 'AGPL-3',
+    'depends': [
+        'project_wbs_element',
     ],
-    "demo": [
+    'data': [
+        'views/analytic_plan_view.xml',
     ],
-    "application": True,
-    "installable": True,
+    'installable': True,
+    'active': False,
 }
--- analytic_plan/models/analytic_plan.py
+++ analytic_plan/models/analytic_plan.py
@@ -5,43 +5,41 @@
 from openerp import fields, models
 
 
-class AccountPlan(models.Model):
+
+class AnalyticPlan(models.Model):
     _name = 'analytic.plan'
     _description = 'Analytic Plan'
 
-    name = fields.Char('Activity description', required=True)
+    name = fields.Char(string='Activity description', required=True)
     date = fields.Date('Date', required=True)
     amount = fields.Float(
-        'Amount', required=True,
+        string='Amount', required=True,
         help=('Calculated by multiplying the quantity '
               'and the price given in the Product\'s '
               'cost price. Always expressed in the '
               'company main currency.'))
     unit_amount = fields.Float(
-        'Quantity',
+        string='Quantity',
         help='Specifies the amount of quantity to count.')
     amount_currency = fields.Float(
-        'Amount Currency',
+        string='Amount Currency',
         help="The amount expressed in an optional other currency.")
-    currency_id = fields.Many2one('res.currency', 'Currency')
+    currency_id = fields.Many2one('res.currency', string='Currency')
     account_id = fields.Many2one(
-        # 'account.analytic.account',
-        'Analytic Account', required=True)
+        string='Analytic Account', required=True)
     user_id = fields.Many2one('res.users', 'User')
     company_id = fields.Many2one(
         'res.company', string='Company',
         readonly=True)
-    product_uom_id = fields.Many2one('product.uom', 'UoM')
-    product_id = fields.Many2one('product.product', 'Product')
+    product_uom_id = fields.Many2one('product.uom', string='UoM')
+    product_id = fields.Many2one('product.product', string='Product')
     general_account_id = fields.Many2one(
         'account.account', string='General Account',
         required=True)
     journal_id = fields.Many2one(
-        # 'account.analytic.plan.journal',
-        'Planning Analytic Journal', required=True)
+        string='Planning Analytic Journal', required=True)
     code = fields.Char()
     ref = fields.Char()
     notes = fields.Text()
     version_id = fields.Many2one(
-        # 'account.analytic.plan.version',
-        'Planning Version', required=True,)
+        string='Planning Version', required=True)
