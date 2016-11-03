7e6c916a88e4e4f37b60d7002b2104c28dd27a37
Hector-Camacho <hectorangelcamachofavela@gmail.com>
2016-11-03 10:49:37 -0600

    [ADD][module] New module was created, the name of the module is task_resource_control and controls the aditive and deductively method

--- task_resource_ control/__openerp__.py
+++ task_resource_ control/__openerp__.py
@@ -3,12 +3,12 @@
 # License LGPL-3.0 or later (https://www.gnu.org/licenses/lgpl.html).
 
 {
-    'name': 'Task Resource',
-    'summary': 'Task Resource',
+    'name': 'Resource control',
+    'summary': 'Resource control',
     'version': '9.0.1.0.0',
     'category': 'Generic Modules',
     'author': (
-        'Eficent Business and IT Consulting Services S.L.,'
+        'Jarsa sistemas S.A de C.V. ,'
         'Odoo Community Association (OCA)'),
     'website': 'https://www.odoo-community.org',
     'license': 'LGPL-3',
@@ -17,13 +17,12 @@
         'product',
         'project_wbs_element',
         'purchase_request_to_rfq',
+        'task_resource',
         ],
 
     'data': [
         'security/ir.model.access.csv',
-        'wizards/purchase_request.xml',
-        'views/total_task_resource_view.xml',
-        'views/task_resource.xml'
+        'views/resource_control_view.xml',
     ],
     'installable': True,
 }
