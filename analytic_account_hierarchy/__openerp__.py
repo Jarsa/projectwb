02775edd96ee15a22c605a692321f61a7d872d87
SaraiOsorio <sarai.osorio@jarsa.com.mx>
2017-05-26 12:59:38 -0500

    [ADD][analytic_account_hierarchy]- Module to create hierarchy in analytical accounts

---
+++ analytic_account_hierarchy/__openerp__.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+# © 2016 Jarsa Sistemas, S.A. de C.V.
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+
+{
+    "name": "Analytic Account Hierarchy",
+    "summary": "Description",
+    "version": "9.0.1.0.0",
+    "category": "Hidden",
+    "website": "https://www.jarsa.com.mx/",
+    "author": "JARSA Sistemas, S.A. de C.V.",
+    "license": "AGPL-3",
+    "installable": True,
+    "depends": [
+        'account', 'analytic'
+    ],
+    "data": [
+        'views/account_analytic_account_view.xml',
+    ],
+    "demo": [
+    ]
+}
