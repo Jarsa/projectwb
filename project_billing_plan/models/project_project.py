2f6880f39cde37044dd78f76c8d9cd31bf2895e8
Alan Ramos <alan.ramos@jarsa.com.mx>
2018-09-09 10:47:44 -0500

    add statics

--- project_billing_plan/models/project_project.py
+++ project_billing_plan/models/project_project.py
@@ -1,7 +1,7 @@
 # Copyright 2018, Jarsa Sistemas, S.A. de C.V.
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 
-from odoo import fields, models
+from odoo import api, fields, models
 
 
 class ProjectProject(models.Model):
@@ -9,3 +9,21 @@ class ProjectProject(models.Model):
 
     income_ids = fields.One2many(
         'project.income', 'project_id', string='Income')
+    total_income = fields.Float(compute='_compute_total_income')
+    total_expense = fields.Float(compute='_compute_total_expenses')
+    balance = fields.Float(compute='_compute_balance')
+
+    @api.multi
+    def _compute_total_income(self):
+        for rec in self:
+            rec.total_income = sum(rec.income_ids.mapped('amount_total'))
+
+    @api.multi
+    def _compute_total_expenses(self):
+        for rec in self:
+            rec.total_expense = sum(rec.task_ids.mapped('subtotal'))
+
+    @api.multi
+    def _compute_balance(self):
+        for rec in self:
+            rec.balance = rec.total_income - rec.total_expense
